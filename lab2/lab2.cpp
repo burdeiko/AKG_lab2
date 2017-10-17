95db457e3d2af072f8ecf7516ee9be6115aa3158
Tsimafei Burdzeika <tymofii.burdeiko@gmail.com>
2017-10-17 10:52:53 +0300

    Fix DrawLine method

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -58,7 +58,50 @@ void DrawPolygon(SDL_Renderer *renderer, SDL_Point *points)
 
 void DrawLine(SDL_Renderer *renderer, int x1, int y1, int x2, int y2)
 {
-	SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+	//SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+	int dx = abs(x2 - x1);
+	int dy = abs(y2 - y1);
+	int sx = x2 >= x1 ? 1 : -1;
+	int sy = y2 >= y1 ? 1 : -1;
+
+	if (dy <= dx)
+	{
+		int d = (dy << 1) - dx;
+		int d1 = dy << 1;
+		int d2 = (dy - dx) << 1;
+
+		SDL_RenderDrawPoint(renderer, x1, y1);
+		for (int x = x1 + sx, y = y1, i = 1; i <= dx; i++, x += sx)
+		{
+			if (d > 0)
+			{
+				d += d2;
+				y += sy;
+			}
+			else
+				d += d1;
+			SDL_RenderDrawPoint(renderer, x, y);
+		}
+	}
+	else
+	{
+		int d = (dx << 1) - dy;
+		int d1 = dx << 1;
+		int d2 = (dx - dy) << 1;
+
+		SDL_RenderDrawPoint(renderer, x1, y1);
+		for (int x = x1, y = y1 + sy, i = 1; i <= dy; i++, y += sy)
+		{
+			if (d > 0)
+			{
+				d += d2;
+				x += sx;
+			}
+			else
+				d += d1;
+			SDL_RenderDrawPoint(renderer, x, y);
+		}
+	}
 }
 
 void GetNextPolygon(SDL_Point *points)
